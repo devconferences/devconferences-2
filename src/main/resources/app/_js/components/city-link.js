9ad0cca94c0d591a02fc06ba4123b7b5077f8306
Sébastien Prunier <sebastien.prunier@gmail.com>
2015-06-10 16:36:40 +0200

    Improve city links positionning

--- src/main/resources/app/_js/components/city-link.js
+++ src/main/resources/app/_js/components/city-link.js
@@ -6,7 +6,7 @@ var Link = Router.Link;
 var CityLink = React.createClass({
     render: function () {
         return (
-            <Link to="city" params={{id: this.props.city.id}} className='btn btn-primary btn-block btn-city'>
+            <Link to="city" params={{id: this.props.city.id}} className='btn btn-primary btn-city'>
                 {this.props.city.name} <span className="badge">{this.props.city.count}</span>
             </Link>
         );
