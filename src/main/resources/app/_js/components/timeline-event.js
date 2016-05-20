e94e76f1563872a393dea4133f790529ef3a2385
Ronan GUILBAULT <ronan.guilbault@gmail.com>
2016-05-20 15:08:05 +0200

    Add FA icon for date
    Signed-off-by: Ronan GUILBAULT <ronan.guilbault@gmail.com>

--- src/main/resources/app/_js/components/timeline-event.js
+++ src/main/resources/app/_js/components/timeline-event.js
@@ -15,7 +15,7 @@ var TimelineEvent = React.createClass({
             var formattedDateBegin = moment(dateBegin).format("DD/MM/YYYY HH:mm");
             if(duration <= 0) {
                 return (
-                    <span>{formattedDateBegin}</span>
+                    <span><i className="fa fa-clock-o"></i> : {formattedDateBegin}</span>
                 );
             } else {
                 var dateEnd = new Date(dateBegin.getTime() + duration);
@@ -26,7 +26,7 @@ var TimelineEvent = React.createClass({
                     formattedDateEnd = moment(dateEnd).format("DD/MM/YYYY HH:mm");
                 }
                 return (
-                    <span>{formattedDateBegin} <i className="fa fa-long-arrow-right"></i> {formattedDateEnd}</span>
+                    <span><i className="fa fa-clock-o"></i> : {formattedDateBegin} <i className="fa fa-long-arrow-right"></i> {formattedDateEnd}</span>
                 );
             }
         };
@@ -63,7 +63,7 @@ var TimelineEvent = React.createClass({
                 var mapsUrl = "http://maps.google.com/?q=" + location.gps.lat + ", " + location.gps.lon;
                 return (
                     <p>
-                        <i className="fa fa-map-marker" aria-hidden="true"></i> : <a href={mapsUrl}>{location.name}</a> ({location.address}, {location.city})
+                        <i className="fa fa-map-marker"></i> : <a href={mapsUrl}>{location.name}</a> ({location.address}, {location.city})
                     </p>
                 );
             } else {
