10a416b8219b73a72b9ef382c3b7c61987a31265
Ronan GUILBAULT <ronan.guilbault@gmail.com>
2016-05-25 17:29:01 +0200

    Minimap can now show cities with a location
    Signed-off-by: Ronan GUILBAULT <ronan.guilbault@gmail.com>

--- src/main/resources/app/_js/components/minimap.js
+++ src/main/resources/app/_js/components/minimap.js
@@ -4,17 +4,34 @@ var Minimap = React.createClass({
 
     render: function() {
         var linkToCity = function(city) {
-            console.log(city);
-            return (
-                <a xlinkHref="city/Nantes" title="This is a link !">
-                    <rect x="100" y="100" width="10" height="10"/>
-                </a>
-            )
+            /*
+             * Equirectangular projection :
+             * North : 51.5500
+             * South : 40.9982
+             * East : 9.9675
+             * West : -5.8125
+             *
+             * With pic of 2,000px * 1,922px (the original) :
+             * cx = (51.55 - ${lon}) / 0.005490
+             * cy = (5.8125 + ${lat}) / 0.007890
+             */
+            if(city.location) {
+                var zoom = 600 / 2000;
+                var cy = parseInt((51.55 - city.location.lat) / (0.005490 / zoom));
+                var cx = parseInt((5.8125 + city.location.lon) / (0.007890 / zoom));
+                return (
+                    <a key={city.id} xlinkHref={"city/" + city.name} title={city.name + "(" + city.count + ")"}>
+                        <ellipse cx={cx + ""} cy={cy + ""} rx="5" ry="5"/>
+                    </a>
+                );
+            } else {
+                return null;
+            }
         }
         return (
             <div className="text-center">
-                <svg width="600" height="600">
-                    <image xlinkHref="/img/france_map.svg" width="600px" height="600px"/>
+                <svg width="600" height="577">
+                    <image xlinkHref="/img/france_map.svg" width="600" height="577"/>
                     {this.props.cities.map(linkToCity)}
                 </svg>
             </div>
