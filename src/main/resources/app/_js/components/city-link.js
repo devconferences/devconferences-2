92faac23224a8564d0e79d129a1b2a497bede445
Ronan GUILBAULT <ronan.guilbault@serli.com>
2016-06-09 11:51:59 +0200

    CityLink show how many conference / community / upcoming event are when hovered
    Signed-off-by: Ronan GUILBAULT <ronan.guilbault@serli.com>

--- src/main/resources/app/_js/components/city-link.js
+++ src/main/resources/app/_js/components/city-link.js
@@ -6,7 +6,10 @@ var Link = Router.Link;
 var CityLink = React.createClass({
     render: function () {
         return (
-            <Link to={`/city/${this.props.city.id}`} className='btn btn-primary btn-city'>
+            <Link to={`/city/${this.props.city.id}`} className='btn btn-primary btn-city'
+                    title={this.props.city.totalConference + " conférence(s), " +
+                           this.props.city.totalCommunity + " communauté(s), " +
+                           this.props.city.totalCalendar + " événement(s)."}>
                 {this.props.city.name} <span className="badge">{this.props.city.count}</span>
             </Link>
         );
